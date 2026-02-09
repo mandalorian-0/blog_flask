ffa0fcb1d2ba993bd0f8a5c9ce51e6378e1f900e
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-02-09 13:03:58 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from flask import Flask, render_template
+
+def get_posts():
+    res = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    res.raise_for_status()
+    data = res.json()
+    return data
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    data = get_posts()
+    return render_template("index.html", posts=data)
+
+@app.route("/posts/<int:id>")
+def all_posts():
+    return render_template("post.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
