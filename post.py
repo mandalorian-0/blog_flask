c4e6defa26c80e55813765b44a5e9bdf31a92a41
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-02-09 15:54:46 -0800

    feat: add post class

--- post.py
+++ post.py
@@ -1,2 +1,11 @@
+import uuid
+import datetime
+
 class Post:
-    pass
+    def __init__(self, title, body, author=None):
+        self.id = str(uuid.uuid4())
+        self.title = title
+        self.author = author
+        self.slug = self.title.replace(" ", "-").lower()
+        self.body = body
+        self.created_at = datetime.datetime.now()
