7e7ca95ba46b0bdf39f03ee558f9aea822d779f9
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-02-09 19:11:20 -0800

    feat: new json

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from post import Post
 
 def get_data():
-    res = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    res = requests.get("https://api.npoint.io/f988fe2f89471cfb558e")
     res.raise_for_status()
     data = res.json()
     return data
@@ -14,17 +14,17 @@ def get_posts():
     posts_data = get_data()
 
     for post in posts_data:
-        post_object = Post(post["title"], post["body"], post["author"])
+        post_object = Post(post["title"], post["body"], post["description"], post["author"])
         posts_objects.append(post_object)
 
     return posts_objects
 
-def get_specific_post(id_lookup: int):
+def get_specific_post(post_slug: str):
     all_posts = get_posts()
     searched_post = {}
 
     for post in all_posts:
-        if post["id"] == id_lookup:
+        if post.slug == post_slug:
             searched_post = post
             break
     return searched_post
@@ -36,9 +36,9 @@ def home():
     data = get_posts()
     return render_template("index.html", posts=data)
 
-@app.route("/posts/<int:post_id>")
-def get_post(post_id):
-    post = get_specific_post(post_id)
+@app.route("/posts/<slug>")
+def get_post(slug: str):
+    post = get_specific_post(slug)
     return render_template("post.html", post=post)
 
 if __name__ == "__main__":
--- post.py
+++ post.py
@@ -2,10 +2,11 @@ import uuid
 import datetime
 
 class Post:
-    def __init__(self, title, body, author=None):
+    def __init__(self, title, body, description=None, author=None):
         self.id = str(uuid.uuid4())
         self.title = title
         self.author = author
         self.slug = self.title.replace(" ", "-").lower()
         self.body = body
+        self.description = description
         self.created_at = datetime.datetime.now()
