b104fd2a856bf72a92347f265ceed32bbfcde186
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-02-15 21:30:20 -0800

    fix: server start error

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from routes.blog_routes import blog_bp
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.register_blueprint(blog_bp)
 
 if __name__ == "__main__":
